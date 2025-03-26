d6a5dfa2975f37872737cfcba795a2f16f493e99
BuildTools <unconfigured@null.spigotmc.org>
2025-03-26 13:10:12 +0100

    Changement de la méthode de créations d'items

--- src/main/java/fr/silenthill99/made_in_abyss_mod/init/ModItems.java
+++ src/main/java/fr/silenthill99/made_in_abyss_mod/init/ModItems.java
@@ -1,6 +1,7 @@
 package fr.silenthill99.made_in_abyss_mod.init;
 
 import fr.silenthill99.made_in_abyss_mod.Main;
+import fr.silenthill99.made_in_abyss_mod.utils.ModSoundEvents;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,18 +14,31 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     static Identifier testId = Identifier.of(Main.MOD_ID, "test");
+    static Identifier sacrificedFlowersId = Identifier.of(Main.MOD_ID, "sacrificed_flowers");
 
     public ModItems() {
 
     }
-    public static final Item TEST = Registry.register(Registries.ITEM, testId, new Item(
+    public static final Item TEST = createItem(testId, new Item(
             new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, testId))
     ));
+
+    public static final Item SACRIFICED_FLOWER = createItem(sacrificedFlowersId, new Item(
+            new Item.Settings()
+                    .jukeboxPlayable(ModSoundEvents.convertToJukeboxSong(ModSoundEvents.SACRIFICED_FLOWERS))
+                    .maxCount(1)
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM, sacrificedFlowersId))
+    ));
+
+    public static <T extends Item> Item createItem(Identifier id, T item) {
+        return Registry.register(Registries.ITEM, id, item);
+    }
 
     public static void registerItems() {
         Main.LOGGER.info("Register mod items for : " + Main.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(TEST);
+            entries.add(SACRIFICED_FLOWER);
         });
     }
 
